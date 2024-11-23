ed750b31ac34f68890a5d1799f3a9ed4b63d826a
ghads001 <undoing-duh-neatly@duck.com>
2024-11-23 16:02:34 +0800

    Create camoufoxtest.py

---
+++ camoufoxtest.py
@@ -0,0 +1,9 @@
+import random
+from camoufox.sync_api import Camoufox
+
+with Camoufox(geoip=True) as browser:
+    page = browser.new_page()
+    page.goto("https://browserscan.net")
+    random_time = random.randint(35, 50)
+    print("Wait " + str(random_time) + " Second！" )
+    page.wait_for_timeout(random_time*1000)
