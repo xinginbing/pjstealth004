82a3c74a992cb5779564cc3cde422d6b6c2c16c9
ghads001 <undoing-duh-neatly@duck.com>
2024-11-24 13:05:17 +0800

    Update camoufoxtest.py

--- camoufoxtest.py
+++ camoufoxtest.py
@@ -1,12 +1,23 @@
+import asyncio
 import random
-from camoufox.sync_api import Camoufox
+import botright
 
-with Camoufox(geoip=True) as browser:
-    page = browser.new_page()
-    page.goto("https://browserscan.net")
+
+async def main():
+    botright_client = await botright.Botright()
+    browser = await botright_client.new_browser()
+    page = await browser.new_page()
+
+    # Continue by using the Page
+    await page.goto("https://browserscan.net")
     random_time = random.randint(35, 50)
-    print("Wait " + str(random_time) + " Second！" )
-    page.wait_for_timeout(random_time*1000)
+	print("Wait " + str(random_time) + " Second！" )
+    await page.wait_for_timeout(random_time*1000)
     texts = page.locator('span').all()
     for t in texts:
         print(t.inner_text())
+    await botright_client.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
