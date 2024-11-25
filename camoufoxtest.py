e4b091c9708173c8629a80a2b96a62957e5d47fd
ghads001 <undoing-duh-neatly@duck.com>
2024-11-25 21:29:54 +0800

    Update camoufoxtest.py

--- camoufoxtest.py
+++ camoufoxtest.py
@@ -1,3 +1,41 @@
+from playwright.sync_api import sync_playwright
+from browserforge.injectors.playwright import NewContext
+from browserforge.fingerprints import FingerprintGenerator
+import random
+
+fingerprints = FingerprintGenerator()
+fingerprints.generate()
+
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch()
+    # Create a new context with the injected fingerprint
+    context = NewContext(browser, fingerprint=fingerprint)
+    page = context.new_page()
+    page.goto("https://browserscan.net")
+    random_time = random.randint(35, 50)
+    print("Wait " + str(random_time) + " Second！" )
+    page.wait_for_timeout(random_time*1000)
+    texts = await page.locator('span').all()
+    for t in texts:
+        print(t.inner_text())
+    browser.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import asyncio
 
 import botright
