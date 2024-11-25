cd6c4074f01a7fd27065dfeb3902b048f4aa1d9b
ghads001 <undoing-duh-neatly@duck.com>
2024-11-25 21:17:34 +0800

    Update camoufoxtest.py

--- camoufoxtest.py
+++ camoufoxtest.py
@@ -1,18 +1,30 @@
-from patchright.sync_api import sync_playwright
-from pjstealth import stealth_sync
+import asyncio
+
+import botright
 import random
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page(
-    user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-    proxy=proxy)
-    stealth_sync(page)
-    page.goto("https://browserscan.net")
+
+async def main():
+    botright_client = await botright.Botright()
+    browser = await botright_client.new_browser()
+    page = await browser.new_page()
+
+    # Continue by using the Page
+    await page.goto("https://browserscan.net")
     random_time = random.randint(35, 50)
     print("Wait " + str(random_time) + " Second！" )
-    page.wait_for_timeout(random_time*1000)
-    texts = page.locator('span').all()
+    await page.wait_for_timeout(random_time*1000)
+    texts = await page.locator('span').all()
     for t in texts:
         print(t.inner_text())
-    browser.close()
+    await botright_client.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+
+
+
+
