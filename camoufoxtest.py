67bdc24a6ead1112adfecf0e9561af43adab920d
ghads001 <undoing-duh-neatly@duck.com>
2024-11-25 21:53:52 +0800

    Update camoufoxtest.py

--- camoufoxtest.py
+++ camoufoxtest.py
@@ -36,33 +36,4 @@ with sync_playwright() as playwright:
 
 
 
-import asyncio
-
-import botright
-import random
-
-
-async def main():
-    botright_client = await botright.Botright()
-    browser = await botright_client.new_browser()
-    page = await browser.new_page()
-
-    # Continue by using the Page
-    await page.goto("https://browserscan.net")
-    random_time = random.randint(35, 50)
-    print("Wait " + str(random_time) + " Second！" )
-    await page.wait_for_timeout(random_time*1000)
-    texts = await page.locator('span').all()
-    for t in texts:
-        print(t.inner_text())
-    await botright_client.close()
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
-
-
-
-
-
 
