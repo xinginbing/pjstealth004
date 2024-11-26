8870b1d6d6c8247e668441601ba1a788c1fd592f
ghads001 <undoing-duh-neatly@duck.com>
2024-11-26 20:39:05 +0800

    Update camoufoxtest.py

--- camoufoxtest.py
+++ camoufoxtest.py
@@ -1,24 +1,22 @@
-from playwright.sync_api import sync_playwright 
-from browserforge.injectors.playwright import NewContext
-from browserforge.fingerprints import FingerprintGenerator
 import random
+from camoufox.sync_api import Camoufox
 
-fingerprint = FingerprintGenerator()
-fingerprint.generate()
-
-with sync_playwright() as playwright:
-    browser = playwright.chromium.launch()
-    # Create a new context with the injected fingerprint
-    context = NewContext(browser, fingerprint=fingerprint)
-    page = context.new_page()
+with Camoufox(
+    geoip=True,
+    proxy={
+        'server': 'http://138.201.49.224:9999',
+        'username': 'johvtmaf53-res-country-US-hold-session-session-6745bf98384fb',
+        'password': 'aqFltrw2g6FdDk8k'
+    }) as browser:
+    page = browser.new_page()
     page.goto("https://browserscan.net")
     random_time = random.randint(35, 50)
-    print("Wait " + str(random_time) + " Second！" )
+    print("Wait " + str(random_time) + " Second!" )
     page.wait_for_timeout(random_time*1000)
     texts = page.locator('span').all()
     for t in texts:
         print(t.inner_text())
-    browser.close()
+	  
 
 
 
