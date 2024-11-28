236d7aaa0ece3e54a3fdb04173bb72107e7699b9
ghads001 <undoing-duh-neatly@duck.com>
2024-11-28 14:53:05 +0800

    Update camoufoxtest.py

--- camoufoxtest.py
+++ camoufoxtest.py
@@ -4,7 +4,9 @@ from camoufox.sync_api import Camoufox
 with Camoufox(
     geoip=True,
     proxy={
-        'server': '89.38.98.115:12825'
+        'server': 'http://136.243.174.185:9999',
+        'username': '4zylzn2nea-res-country-US-state-4896861-city-4556232-hold-hardsession-session-674810e5a43df',
+        'password': '0EF1FRQg9N9G3c8G'
     }) as browser:
     page = browser.new_page()
     page.goto("https://browserscan.net")
