e8254103fb97758e504372b4764d4893855ed4e7
ghads001 <undoing-duh-neatly@duck.com>
2024-12-02 19:59:53 +0800

    Update camoufoxtest.py

--- camoufoxtest.py
+++ camoufoxtest.py
@@ -1,21 +1,18 @@
-import random
-from camoufox.sync_api import Camoufox
+import requests
 
-with Camoufox(
-    geoip=True,
-    proxy={
-        'server': 'http://136.243.174.185:9999',
-        'username': '4zylzn2nea-corp.mobile.res-country-RU-state-479119-city-541722-hold-session-session-67481517caddf',
-        'password': '0EF1FRQg9N9G3c8G'
-    }) as browser:
-    page = browser.new_page()
-    page.goto("https://browserscan.net")
-    random_time = random.randint(35, 50)
-    print("Wait " + str(random_time) + " Second!" )
-    page.wait_for_timeout(random_time*1000)
-    texts = page.locator('span').all()
-    for t in texts:
-        print(t.inner_text())
+url = 'https://125700.shop/ecdd30e7e8f019717fb8/2643f1413f/?placementName=default'
+apikey = '9ff6d18370ad67f79b0193ccf8c9145bc608a3b9'
+params = {
+    'url': url,
+    'apikey': apikey,
+	'js_render': 'true',
+	'premium_proxy': 'true',
+	'proxy_country': 'us',
+	'wait': '35000',
+	'session_id': '22345',
+}
+response = requests.get('https://api.zenrows.com/v1/', params=params)
+print(response.text)
 	  
 
 
