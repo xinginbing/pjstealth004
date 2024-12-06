a393d2a02883756538f6ec3a0a4293a5c255b035
ghads001 <undoing-duh-neatly@duck.com>
2024-12-06 16:19:09 +0800

    Update camoufoxtest.py

--- camoufoxtest.py
+++ camoufoxtest.py
@@ -4,13 +4,13 @@ import random
 
 token = "54e88bca11504c90b8a184e06c3d89ac7814f7e9a4d"
 
-link = "https://luglawhaulsano.net/4/8605030"
+link = "https://httpbin.co/anything"
 
 targetUrl = urllib.parse.quote(link)
 
-#super = "true"
+super = "true"
 
-geoCode = "de"
+geoCode = "us"
 
 #regionalGeoCode = "europe"
 #asia 亚洲 europe 欧洲 africa 非洲  oceania 大洋洲 northamerica 北美 southamerica 南美洲  
@@ -29,8 +29,8 @@ width = "720"
 
 blockResources = "false"
 
-#url = "http://api.scrape.do?token={}&url={}&super={}&geoCode={}&sessionId={}&timeout={}&device={}&customWait={}&render={}&blockResources={}&height={}&width={}".format(token, targetUrl, super, geoCode, sessionId, timeout, device, customWait, render, blockResources, height, width)
-url = "http://api.scrape.do?token={}&url={}&geoCode={}&sessionId={}&timeout={}&device={}&customWait={}&render={}&blockResources={}&height={}&width={}".format(token, targetUrl, geoCode, sessionId, timeout, device, customWait, render, blockResources, height, width)
+url = "http://api.scrape.do?token={}&url={}&super={}&geoCode={}&sessionId={}&timeout={}&device={}&customWait={}&render={}&blockResources={}&height={}&width={}".format(token, targetUrl, super, geoCode, sessionId, timeout, device, customWait, render, blockResources, height, width)
+#url = "http://api.scrape.do?token={}&url={}&geoCode={}&sessionId={}&timeout={}&device={}&customWait={}&render={}&blockResources={}&height={}&width={}".format(token, targetUrl, geoCode, sessionId, timeout, device, customWait, render, blockResources, height, width)
 response = requests.request("GET", url)
 
 print(response.text)
