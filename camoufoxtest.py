56267142d3837fd8d92d795632b3b9613aadfdb4
ghads001 <undoing-duh-neatly@duck.com>
2024-12-07 16:14:50 +0800

    Update camoufoxtest.py

--- camoufoxtest.py
+++ camoufoxtest.py
@@ -3,15 +3,18 @@ import urllib.parse
 
 token = "54e88bca11504c90b8a184e06c3d89ac7814f7e9a4d"
 targetUrl = urllib.parse.quote("https://httpbin.co/headers")
-customHeaders = "true"
-payload = {}
-url = "http://api.scrape.do?token={}&url={}&customHeaders={}".format(token, targetUrl, customHeaders)
-headers = {
-  'Test-Header-Key': 'TestValue',
-  "Sec-Ch-Ua-Mobile": "?1",
-  "Sec-Ch-Ua-Platform": "\"android\""
-}
-response = requests.request("GET", url, headers=headers, data=payload)
+#customHeaders = "true"
+#payload = {}
+device = 'mobile'
+#url = "http://api.scrape.do?token={}&url={}&customHeaders={}".format(token, targetUrl, customHeaders)
+url = "http://api.scrape.do?token={}&url={}&device={}".format(token, targetUrl, device)
+#headers = {
+#  'Test-Header-Key': 'TestValue',
+#  "Sec-Ch-Ua-Mobile": "?1",
+#  "Sec-Ch-Ua-Platform": "\"android\""
+#}
+#response = requests.request("GET", url, headers=headers, data=payload)
+response = requests.request("GET", url)
 print(response.text)
 
 
